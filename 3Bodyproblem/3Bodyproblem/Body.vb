6185f4944ffcc9e0b4b14995778568e12d4c8fa3
DeltaDizzy <EJBraswell@gmail.com>
2019-12-04 11:06:28 -0600

    Add files via upload

---
+++ 3Bodyproblem/3Bodyproblem/Body.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Drawing.Color
+Public Class Body
+    Dim G As Double = 0.00000000006679 ' universal grav constant
+    Public px, py As Double ' x and y positions
+    Dim vx, vy As Double ' x and y velocities
+    Dim fx, fy As Double ' forces
+    Dim mass As Double ' body mass
+    Dim size As Double
+    Dim r2 As Double
+    Dim r3 As Double
+    Dim color As Color
+
+    Public Sub New(rx As Double, ry As Double, vx As Double, vy As Double, mass As Double, color As Color)
+        Me.px = rx
+        Me.py = ry
+        Me.vx = vx
+        Me.vy = vy
+        Me.mass = mass
+        Me.color = color
+    End Sub
+
+    Public Sub update(dt As Double)
+        r2 = px * px + py * py
+        r3 = r2 * Math.Sqrt(r2)
+
+        fx = -px / r3
+        fy = -py / r3
+
+        px += vx * dt
+        py += vy * dt
+
+        vx += fx * dt
+        vy += fy * dt
+
+    End Sub
+
+    Public Sub distanceTo(b As Body)
+
+    End Sub
+End Class
---
+++ 3Bodyproblem/3Bodyproblem/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim graph As Graphics = Me.CreateGraphics()
+    Dim pen As New System.Drawing.Pen(Brushes.Black)
+    Dim bodies As List(Of Body) = New List(Of Body)
+    Dim b1, b2, b3 As Body
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        b1 = New Body(100, 100, 0, 0, 12, Color.Blue)
+        b2 = New Body(200, 150, 0, 0, 14, Color.Brown)
+        b3 = New Body(300, 90, 0, 0, 13, Color.Yellow)
+        bodies.Add(b1)
+        bodies.Add(b2)
+        bodies.Add(b3)
+    End Sub
+
+    Private Sub draw(b As Body)
+        graph.DrawEllipse(pen, CSng(b.px), CSng(b.py), 7, 7)
+    End Sub
+    Private Sub TmrIntegrator_Tick(sender As Object, e As EventArgs) Handles tmrIntegrator.Tick
+        lblCOords.Text = Cursor.Position.ToString
+        If bodies.Count > 0 Then
+            For Each b As Body In bodies
+                draw(b)
+            Next
+        End If
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+    End Sub
+End Class
